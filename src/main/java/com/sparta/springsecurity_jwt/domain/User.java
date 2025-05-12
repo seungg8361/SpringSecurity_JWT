f88fc10e6e0b91fd6b77709f4a8591625fa24a80
SeungGyu <seungg8361@naver.com>
2025-05-12 22:18:29 +0900

    feat : User 엔티티 생성 및 JpaRepository를 상속받은 repository 구성

---
+++ src/main/java/com/sparta/springsecurity_jwt/domain/User.java
@@ -0,0 +1,42 @@
+package com.sparta.springsecurity_jwt.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    public static User create(String username, String password, String nickname) {
+        return User.builder()
+                .username(username)
+                .password(password)
+                .nickname(nickname)
+                .role(UserRole.USER)
+                .build();
+    }
+}
