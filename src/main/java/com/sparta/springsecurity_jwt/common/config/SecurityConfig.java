2d30cc6e07d73c5c25c11effe16255cbcabead50
SeungGyu <seungg8361@naver.com>
2025-05-12 22:20:26 +0900

    feat : Security 설정

---
+++ src/main/java/com/sparta/springsecurity_jwt/common/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.sparta.springsecurity_jwt.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.headers(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(authorizeRequests -> {
+            authorizeRequests.
+                    requestMatchers("api/v1/users/**").permitAll()
+                    .requestMatchers("/h2-console/**").permitAll()
+                    .anyRequest().authenticated();
+        });
+
+        http.formLogin(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
